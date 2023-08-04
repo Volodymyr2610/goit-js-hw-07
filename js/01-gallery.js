93bacfd7ade8cc78b78f3cbc588eb5b58a8b96b3
Volodymyr2610 <129291672+Volodymyr2610@users.noreply.github.com>
2023-08-04 14:33:37 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 console.log(basicLightbox);
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(({preview, original, description}) => 
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`)  
+.join("");
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup)
+
+galleryRef.addEventListener("click", openOriginalImage);
+
+function openOriginalImage(event){
+  if(event.target.nodeName !== 'IMG') {
+    return 
+  };
+  event.preventDefault();
+   basicLightbox.create(`<img src="${event.target.dataset.source}" alt = "${event.target.alt}"/>`, {
+    className: 'basicLightbox',
+    onShow: (basicLightbox) => {window.addEventListener('keydown', (event) => {
+      if(event.code === 'Escape')basicLightbox.close()})},
+    onClose: (basicLightbox) => {window.removeEventListener('keydown', (event) => {basicLightbox.close()})}
+  }).show()
+     
+}
